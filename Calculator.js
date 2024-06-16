6d694baf2e2c27e0ca6a506347728efa7cfa9865
Arkeiash <samsooter300@gmail.com>
2024-06-16 13:46:55 -0700

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -8,7 +8,7 @@ let gamma = function(n) {
         var g = 7, // g represents the precision desired, p is the values of p[i] to plug into Lanczos' formula
             p = [0.99999999999980993, 676.5203681218851, -1259.1392167224028, 771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
         if (n < 0.5) {
-            return Math.PI / Math.sin(n * Math.PI) / this.gamma(1 - n); // Added 'this' to gamma call
+            return Math.PI / Math.sin(n * Math.PI) / gamma(1 - n); // Added 'this' to gamma call
         } else {
             n--;
             var x = p[0];
@@ -20,7 +20,7 @@ let gamma = function(n) {
         }
     }
 let factorial = function(number) {
-        if(number % 1 != 1) {return this.gamma(number +1);}
+        if(number % 1 != 1) {return gamma(number +1);}
         if(number === 0 || number === 1) {
             return 1;
         }
