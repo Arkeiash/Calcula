4d2bafd08e920cedd1e8f90aaa282ec02fe0e6fb
Arkeiash <samsooter300@gmail.com>
2024-06-20 15:21:19 -0700

    Update Person.js

--- Person.js
+++ Person.js
@@ -43,7 +43,8 @@ class Person extends GameObject {
       state.map.moveWall(this.x, this.y, this.direction);
       this.movingProgressRemaining = 16;
       this.updateSprite(state);
-      if(state.who.id === "hero") {
+      
+      if(this.id === "hero") {
         state.map.updateChunks(this.x, this.y);
       }
     
