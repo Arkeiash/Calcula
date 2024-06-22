6b07b8d25bbaf2a9aaefca19aaf88c25ad8e9090
Arkeiash <samsooter300@gmail.com>
2024-06-22 14:45:25 -0700

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,23 +1,27 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Creating local variables to hold form data
-    $comment = isset($_POST['comment']) ? htmlspecialchars(trim($_POST['comment'])) : '';
-    $name = isset($_POST['name']) ? htmlspecialchars(trim($_POST['name'])) : '';
-    // Validate that none of the fields are empty
-    if (!empty($comment)) {
-        $to = 'simonsooter300@gmail.com'; // Replace with your email address
-        $subject = 'New Form Submission';
-        $message = "Name: $name\n Comment:\n$comment";
+<?php 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
+    $comment = isset($_POST['comment']) ? htmlspecialchars($_POST['comment']) : '';
 
+    $servername = "localhost";
+    $username = "calcula_admin";
+    $password = "Vorgrastao6:12";
+    $dbname = "calcula_database";
 
-        // Send the email
-        if (mail($to, $subject, $message)) {
-            echo "Your message has been sent.";
-        } else {
-            echo "There was a problem sending your message.";
-        }
-    } else {
-        echo "All fields are required.";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+    $sql = "INSERT INTO comments (name, comment) VALUES ('$name', '$comment')";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close connection
+    $conn->close();
+
+
 }
-?>
